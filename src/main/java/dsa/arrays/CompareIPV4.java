6b76bdb195760da3e406c2da4a51bd69846bd345
Kunal Sharma <kos26@drexel.edu>
2025-10-23 13:35:49 -0400

    Solved easy array problem.

---
+++ src/main/java/dsa/arrays/CompareIPV4.java
@@ -0,0 +1,42 @@
+package dsa.arrays;
+
+/*
+    Not a leetcode problem, this was asked in one of the first coding interview questions I tried solving during a live interview.
+
+    Problem: Given two strings 111.222.33.5 and 2.4.6.2, return the larger ip address.
+    Note, a string could be something like 111.222, we should consider it valid and if compared with 110.222.000.1,
+    should return 111.222 as the larger ip address
+ */
+public class CompareIPV4 {
+    public String solution(String ip1, String ip2){
+        int[] ipBlock1 = splitIp(ip1);
+        int[] ipBlock2 = splitIp(ip2);
+
+        return compareBlocks(ipBlock1, ipBlock2) ? ip1 : ip2;
+    }
+
+    private int[] splitIp(String ip){
+        int[] ipBlock = {-1, -1, -1, -1};
+        String[] block = ip.split("\\.");
+
+        for (int i = 0; i < block.length; i++){
+            try {
+                ipBlock[i] = Integer.parseInt(block[i]);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("IPv4 is invalid");
+            }
+        }
+
+        return ipBlock;
+    }
+
+    private boolean compareBlocks(int[] ip1, int[] ip2){
+        for (int i = 0; i < 4; i++) {
+            if (ip1[i] == ip2[i]) {
+                continue;
+            }
+            return ip1[i] > ip2[i];
+        }
+        return true;
+    }
+}
---
+++ src/main/java/dsa/bit_manipulation/AddBinary.java
@@ -0,0 +1,12 @@
+package dsa.bit_manipulation;
+
+/**
+ * Title: Add Binary (LeetCode 67)   Topic: Bit Manipulation   Difficulty: Easy
+ * Link: https://leetcode.com/problems/add-binary/description/?envType=study-plan-v2&envId=top-interview-150
+ *
+ * <p>
+ * Problem:
+ * Given two binary strings a and b, return their sum as a binary string.
+ */
+public class AddBinary {
+}
