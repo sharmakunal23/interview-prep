44f3418e078a098223035eb00f3543c06580f88b
Kunal Sharma <kos26@drexel.edu>
2025-11-10 19:04:34 -0500

    problem(Invert Binary Tree): leetcode problem 226
    https://leetcode.com/problems/invert-binary-tree/description/

--- src/main/java/dsa/binary_tree_general/InvertBinaryTree.java
+++ src/main/java/dsa/binary_tree_general/InvertBinaryTree.java
@@ -19,10 +19,14 @@ public class InvertBinaryTree {
         if (root == null) {
             return null;
         }
-        TreeNode left = invertTree(root.left);
-        TreeNode right = invertTree(root.right);
-        root.left = right;
-        root.right = left;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
         return root;
     }
 }
